196fdcbf9594e2f2c1d4e108d86c46ab191c1b46
Clyde Daniel <cdaniel@sentryone.com>
2018-06-19 13:32:21 -0400

    base setup

---
+++ movies_search/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="row">
+              <div className="row-sm-6">
+                <i className="fa fa-film"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-12">
+            cell 2
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
